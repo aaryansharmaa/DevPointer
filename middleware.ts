46e4f2833cf1fcd904d06c350a82db30d9512f6a
aaryansharmaa <aaryans018@gmail.com>
2024-05-01 14:24:00 +0530

    more middle

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/question/:id",
-  "/tags",
-  "/profile/:id",
+// Define public routes where no authentication is needed
+const isPublicRoute = createRouteMatcher([
+  "/", // Home page is public
+  "/api/webhook", // Webhook should be accessible without authentication
+  "/question/(.*)", // Question details are public
+  "/tags", // Listing of tags is public
+  "/tags/(.*)", // Specific tag details are public
+  "/community", // Community page is public
+  "/jobs",
+  "/api/chatgpt", // Jobs listing is public
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  // Protect routes that are not explicitly public
+  if (!isPublicRoute(req)) {
+    auth().protect(); // Redirect unauthenticated users to sign-in
+  }
 });
 
 export const config = {
-  matcher: ["/((?!...|_next).)", "/", "/(api|trpc)(.)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
