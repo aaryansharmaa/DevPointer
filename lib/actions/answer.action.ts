461be36baff24b37619cbacfcc4d3cc07286e8ea
aaryansharmaa <aaryans018@gmail.com>
2024-05-14 18:38:33 +0530

    finish q&a edit

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -7,8 +7,11 @@ import { connectToDatabase } from "../database/mongoose";
 import {
   AnswerVoteParams,
   CreateAnswerParams,
+  DeleteAnswerParams,
   GetAnswersParams,
 } from "./shared.types";
+import { Tag } from "lucide-react";
+import Interaction from "../database/interaction.model";
 
 export async function createAnswer(params: CreateAnswerParams) {
   try {
@@ -118,5 +121,26 @@ export async function downvoteAnswer(params: AnswerVoteParams) {
   } catch (error) {
     console.log(error);
     throw error;
+  }
+}
+
+export async function deleteAnswer(params: DeleteAnswerParams) {
+  try {
+    connectToDatabase();
+    const { answerId, path } = params;
+    const answer = await Answer.findById(answerId);
+
+    if (!answer) {
+      throw new Error("Answer not found");
+    }
+    await Question.updateMany(
+      { _id: answer.question },
+      { $pull: { answers: answerId } }
+    );
+    await Interaction.deleteMany({ answer: answerId });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
   }
 }
