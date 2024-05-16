dd81f0dfcb86f954b465bd5d6ec129488ec1a3f6
aaryansharmaa <aaryans018@gmail.com>
2024-05-16 17:21:58 +0530

    filtering done

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -13,7 +13,7 @@ interface Props {
   userId: string;
   totalAnswers: number;
   page?: number;
-  filter?: number;
+  filter?: string;
 }
 
 const AllAnswers = async ({
@@ -23,7 +23,11 @@ const AllAnswers = async ({
   page,
   filter,
 }: Props) => {
-  const answer = await getAnswers({ questionId });
+  const answer = await getAnswers({
+    questionId,
+    page: page ? +page : 1,
+    sortBy: filter,
+  });
   return (
     <div className="mt-11">
       <div className="flex items-center justify-between">
