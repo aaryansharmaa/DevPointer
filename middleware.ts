3cef7d6c00bb9464b872a8966aee29c6133d48ca
aaryansharmaa <aaryans018@gmail.com>
2024-05-27 14:07:48 +0530

    auth change last

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const protectedRoutes = createRouteMatcher(["/ask-question", "/collection", "/community", "/profile", "/appquestion", "/tags"]);
+// Use a regex literal to match dynamic routes
+const protectedRoutes = createRouteMatcher([
+  "/ask-question",
+  "/collection",
+  "/community",
+  "/profile",
+  "/question",
+  "/tags",
+  /^\/question\/[^/]+$/  // Regex literal to match /question/[id] pattern
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (protectedRoutes(req)) auth().protect();
