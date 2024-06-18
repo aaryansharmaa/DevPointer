7cf3c35374277b0b03d3bacb453b79f82a2d8579
aaryansharmaa <aaryans018@gmail.com>
2024-06-18 11:01:39 +0530

    hydration error fixed

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,3 +1,5 @@
+"use server";
+
 import Link from "next/link";
 import Image from "next/image";
 import { getTopInteractedTags } from "@/lib/actions/tag.action";
@@ -13,43 +15,41 @@ interface Props {
     username: string;
   };
 }
+
 const UserCard = async ({ user }: Props) => {
   const interactedTags = await getTopInteractedTags({ userId: user._id });
-  return (
-    <Link
-      href={`/profile/${user.clerkId}`}
-      className="shadow-light100_darknone w-full max-xs:min-w-full"
-    >
-      <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
-        <Image
-          src={user.picture}
-          alt="user profile picture"
-          width={100}
-          height={100}
-          className="rounded-full"
-        />
 
-        <div className="mt-4 text-center">
-          <h3 className="h3-bold text-dark200_light900 line-clamp-1">
-            {user.name}
-          </h3>
-          <p className="body-regular text-dark500_light500 mt-2">
-            @{user.username}
-          </p>
-        </div>
-        <div className="mt-5">
-          {interactedTags.length > 0 ? (
-            <div className="flex items-center gap-2">
-              {interactedTags.map((tag) => (
-                <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
-              ))}
-            </div>
-          ) : (
-            <Badge>No tags yet</Badge>
-          )}
-        </div>
-      </article>
-    </Link>
+  return (
+    <div className="shadow-light100_darknone w-full max-xs:min-w-full">
+      <Link href={`/profile/${user.clerkId}`} className="no-underline">
+        <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
+          <Image
+            src={user.picture}
+            alt="user profile picture"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+          <div className="mt-4 text-center">
+            <h3 className="h3-bold text-dark200_light900 line-clamp-1">
+              {user.name}
+            </h3>
+            <p className="body-regular text-dark500_light500 mt-2">
+              @{user.username}
+            </p>
+          </div>
+        </article>
+      </Link>
+      <div className="mt-5 flex items-center gap-2 justify-center">
+        {interactedTags.length > 0 ? (
+          interactedTags.map((tag) => (
+            <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
+          ))
+        ) : (
+          <Badge>No tags yet</Badge>
+        )}
+      </div>
+    </div>
   );
 };
 
--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -11,15 +11,16 @@ interface Props {
 
 const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
   return (
-    <Link href={`/tags/${_id}`} className="flex justify-between gap-2">
-      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
-        {name}
-      </Badge>
-
+    <div className="flex justify-between gap-2 items-center">
+      <Link href={`/tags/${_id}`} className="no-underline">
+        <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+          {name}
+        </Badge>
+      </Link>
       {showCount && (
         <p className="small-medium text-dark500_light700">{totalQuestions}</p>
       )}
-    </Link>
+    </div>
   );
 };
 
